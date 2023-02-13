032541c89f7d008a72fa246fa574fba8b44f1e21
Graybound <66386662+Graybound@users.noreply.github.com>
2023-02-13 07:10:26 -0600

    Update file_ops.py
    Deleted unneeded comments

--- Python/Week 2 File manipulation project/file_ops.py
+++ Python/Week 2 File manipulation project/file_ops.py
@@ -1,19 +1,5 @@
 def read_file(file_name):
-    """ Reads in a file.
-
-    [IMPLEMENT ME]
-        1.  Open and read the given file into a variable using the File read()
-            function
-        2.  Print the contents of the file
-        3.  Return the contents of the file
-
-    Args:
-        file_name: the name of the file to be read
-
-    Returns:
-        string: contents of the given file.
-    """
-    # WRITE SOLUTION HERE
+   
     try:
         with open(file_name, 'r') as file:
             data = file.read()
@@ -24,20 +10,7 @@ def read_file(file_name):
 
 
 def read_file_into_list(file_name):
-    """ Reads in a file and stores each line as an element in a list
-
-    [IMPLEMENT ME]
-        1. Open the given file
-        2. Read the file line by line and append each line to a list
-        3. Return the list
-
-    Args:
-        file_name: the name of the file to be read
-
-    Returns:
-        list: a list where each element is a line in the file.
-    """
-    # WRITE SOLUTION HERE
+  
     try:
         with open(file_name, 'r') as file:
             data = file.readlines()
@@ -48,21 +21,7 @@ def read_file_into_list(file_name):
 
 
 def write_first_line_to_file(file_contents, output_filename):
-    """ Writes the first line of a string to a file.
-
-    [IMPLEMENT ME]
-        1.  Get the first line of file_contents
-        2.  Use the File write() function to write the first line into a file
-            with the name from output_filename
-
-        We determine the first line to be everything in a string before the
-        first newline ('\n') character.
-
-    Args:
-        file_contents: string to be split and written into output file
-        output_filename: the name of the file to be written to
-    """
-    # WRITE SOLUTION HERE
+    
     try:
         content_list = file_contents.split('\n')
         first_line = content_list[0]
@@ -74,20 +33,7 @@ def write_first_line_to_file(file_contents, output_filename):
 
 
 def read_even_numbered_lines(file_name):
-    """ Reads in the even numbered lines of a file
-
-    [IMPLEMENT ME]
-        1. Open and read the given file into a variable
-        2. Read the file line-by-line and add the even-numbered lines to a list
-        3. Return the list
-
-    Args:
-        file_name: the name of the file to be read
-
-    Returns:
-        list: a list of the even-numbered lines of the file
-    """
-    # WRITE SOLUTION HERE
+   
     try:
         with open(file_name, 'r+') as file:
             data = file.readlines()
@@ -107,21 +53,7 @@ def read_even_numbered_lines(file_name):
 
 
 def read_file_in_reverse(file_name):
-    """ Reads a file and returns a list of the lines in reverse order
-
-    [IMPLEMENT ME]
-        1. Open and read the given file into a variable
-        2. Read the file line-by-line and store the lines in a list in reverse order
-        3. Print the list
-        4. Return the list
-
-    Args:
-        file_name: the name of the file to be read
-
-    Returns:
-        list: list of the lines of the file in reverse order.
-    """
-    # WRITE SOLUTION HERE
+   
     try:
         with open(file_name, 'r') as file:
             data = file.readlines()
@@ -132,13 +64,6 @@ def read_file_in_reverse(file_name):
 
     except Exception as e:
         pass  # print("Error in 'read_file_in_reverse' function; \n",e)
-
-
-'''
-Here are some sample commands to help you run/test your implementations.
-Feel free to uncomment/modify/add to them as you wish.
-'''
-
 
 def main():
     file_contents = read_file("sampletext.txt")
